f02d3ff04c88913a4162e82ae09463059b90dd8c
Andres Bayona <al.bayona@uniandes.edu.co>
2019-10-09 20:36:09 -0500

    Changes

--- src/model/logic/MVCModelo.java
+++ src/model/logic/MVCModelo.java
@@ -25,12 +25,10 @@ public class MVCModelo {
     public final static String DATOS_SEGUNDO_SEMESTRE = "./data/bogota-cadastral-2018-2-WeeklyAggregate.csv";
     public final static String DATOS_TERCER_SEMESTRE = "./data/bogota-cadastral-2018-3-WeeklyAggregate.csv";
     public final static String DATOS_CUARTO_SEMESTRE = "./data/bogota-cadastral-2018-4-WeeklyAggregate.csv";
-    public final static int CANTIDAD_EXISTENTE = 8000;
-    public final static int CANTIDAD_NO_EXISTENTE = 2000;
+    public final static int CANTIDAD_EXISTENTE = 30;
+    public final static int CANTIDAD_NO_EXISTENTE = 10;
 
     private ListaEncadenada<TravelTime> datos;
-    private TravelTime[] muestraAleatorea;
-
     private TablaHashSeparateChaining<String, ListaEncadenada<TravelTime>> tablaHashSC;
     private TablaHashLinearProbing<String, ListaEncadenada<TravelTime>> tablaHashLP;
 
@@ -44,15 +42,21 @@ public class MVCModelo {
     }
 
 
-    public void loadTravelTimes(){
-        cargar(DATOS_PRIMER_SEMESTRE, 1);
-        cargar(DATOS_SEGUNDO_SEMESTRE, 2);
-        cargar(DATOS_TERCER_SEMESTRE, 3);
-        cargar(DATOS_CUARTO_SEMESTRE, 4);
+    public String loadTravelTimes(){
 
+        String info =   cargar(DATOS_PRIMER_SEMESTRE, 1) + cargar(DATOS_SEGUNDO_SEMESTRE, 2)+ cargar(DATOS_TERCER_SEMESTRE, 3) + cargar(DATOS_CUARTO_SEMESTRE, 4);
+
+        cargarTablaLinearProbing();
+        cargarTablaHashSeparateChaining();
+
+        return info;
     }
 
-    public void cargar(String ruta, int semestre) {
+    public String cargar(String ruta, int trimestre) {
+
+       TravelTime primero = null;
+       int cant = datos.tamano();
+
         CSVReader reader = null;
         try {
 
@@ -66,7 +70,9 @@ public class MVCModelo {
 
                 String[] parametros = (String[]) iter.next();
 
-                TravelTime v = crearViaje(parametros, semestre);
+                TravelTime v = crearViaje(parametros, trimestre);
+
+                if (primero == null) primero =v;
 
                 agregar(v);
             }
@@ -83,6 +89,12 @@ public class MVCModelo {
             }
 
         }
+
+        return  "Para el  trimestre " + trimestre + " del 2018 se tiene la siguiente informacion: \n" + "Primer viaje: Origen: " +primero.darSourceId()+ ", Destino: " + primero.darDistId() + ", Dia: " + primero.darDow() + ", Tiempo promedio: " + primero.darMeanTravelTime() + "\n" +
+
+          "Ultimo viaje: Origen: " + datos.ultimo().darSourceId() + ", Destino: " + datos.ultimo().darDistId() + ", Dia: " + datos.ultimo().darDow() + ", Tiempo promedio: " + datos.ultimo().darMeanTravelTime() + "\n " +
+               "Total de viajes leidos: " + (datos.tamano() - cant) + "\n \n";
+
     }
 
 
@@ -133,7 +145,7 @@ public class MVCModelo {
 
         int cont = 0;
 
-        Iterator<TravelTime> iter = lista.iterador();
+        Iterator<TravelTime> iter = lista.iterator();
 
         while(iter.hasNext())
         {
@@ -168,7 +180,7 @@ public class MVCModelo {
         tablaHashSC = new TablaHashSeparateChaining<>();
 
 
-        Iterator<TravelTime> iter = datos.iterador();
+        Iterator<TravelTime> iter = datos.iterator();
 
         while(iter.hasNext())
         {
@@ -193,7 +205,7 @@ public class MVCModelo {
 
         tablaHashLP = new TablaHashLinearProbing<>();
 
-        Iterator<TravelTime> iter = datos.iterador();
+        Iterator<TravelTime> iter = datos.iterator();
 
         while(iter.hasNext())
         {
@@ -228,7 +240,7 @@ public class MVCModelo {
     public ListaEncadenada<TravelTime> buscarPorTrimestreOrigenYDestinoSC(int tri, int org, int dst) {
 
         ListaEncadenada<TravelTime> lista = tablaHashSC.get(tri + "-" + org + "-" + dst);
-        lista.ordenarPorMergeSort(new ComparadorPorDia());
+        if (lista != null) lista.ordenarPorMergeSort(new ComparadorPorDia());
 
         return lista;
 
@@ -238,7 +250,7 @@ public class MVCModelo {
     public ListaEncadenada<TravelTime> buscarPorTrimestreOrigenYDestinoLP(int tri, int org, int dst) {
 
         ListaEncadenada<TravelTime> lista = tablaHashLP.get(tri + "-" + org + "-" + dst);
-        lista.ordenarPorMergeSort(new ComparadorPorDia());
+        if (lista != null) lista.ordenarPorMergeSort(new ComparadorPorDia());
 
         return lista;
     }
@@ -254,7 +266,7 @@ public class MVCModelo {
 
     public String tiemposGetSC(){
 
-        TravelTime[] muestraAleatroea = generarMuestraAleatorea();
+        TravelTime[] muestraAleatorea = generarMuestraAleatorea();
 
         double max = 0;
         double min = Double.MAX_VALUE;
@@ -275,14 +287,14 @@ public class MVCModelo {
 
         }
 
-        promedio = total / (CANTIDAD_NO_EXISTENTE + CANTIDAD_EXISTENTE)
+        promedio = total / (CANTIDAD_NO_EXISTENTE + CANTIDAD_EXISTENTE);
 
-        return "Max: " + max + " Min: " + min + " Prom: " + promedio ;
+        return "Max: " + max + " ms,  Min: " + min + " ms, Prom: " + promedio +" ms";
     }
 
-    public String tiemposGetSC(){
+    public String tiemposGetLP(){
 
-        TravelTime[] muestraAleatroea = generarMuestraAleatorea();
+        TravelTime[] muestraAleatorea = generarMuestraAleatorea();
 
         double max = 0;
         double min = Double.MAX_VALUE;
@@ -303,9 +315,9 @@ public class MVCModelo {
 
         }
 
-        promedio = total / (CANTIDAD_NO_EXISTENTE + CANTIDAD_EXISTENTE)
+        promedio = total / (CANTIDAD_NO_EXISTENTE + CANTIDAD_EXISTENTE);
 
-        return "Max: " + max + " Min: " + min + " Prom: " + promedio ;
+        return "Max: " + max + " ms,  Min: " + min + " ms, Prom: " + promedio +" ms";
     }
 
 
@@ -327,8 +339,12 @@ public class MVCModelo {
         {
             MVCModelo modelo = new MVCModelo ();
             modelo.loadTravelTimes();
-            System.out.println("Tiempo de agregar para cola: " +modelo.tiempoPromedioAgregarImplementacionCola());
-            
+            modelo.cargarTablaHashSeparateChaining();
+            modelo.cargarTablaLinearProbing();
+            ListaEncadenada lista = modelo.buscarPorTrimestreOrigenYDestinoLP(1,1,2);
+            System.out.println("Tiempo de agregar para SC Hash Table: " +modelo.tiemposGetSC());
+            System.out.println("Tiempo de agregar para LC Hash Table: " +modelo.tiemposGetLP());
+
         }
 
 }
